d6f9a1d39270c83b14a7792aea5bb3d949237ab3
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 11:20:21 -0700

    Updated User class to save messages. Created Message kind (class)

--- models.py
+++ models.py
@@ -2,3 +2,10 @@ from google.appengine.ext import ndb
 
 class User(ndb.Model):
     user_name = ndb.StringProperty()
+    messages = ndb.KeyProperty(repeated = True)
+
+class Message(ndb.Model):
+    # CHARACTER_LIMIT = 200
+    message_txt = ndb.TextProperty()
+    sender = ndb.KeyProperty()
+    reciever = ndb.KeyProperty()
