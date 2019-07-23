e561b8925a875a0c6741d967187bb9ee19a21f49
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-23 12:15:53 -0700

    Created all handlers.

--- main.py
+++ main.py
@@ -19,7 +19,36 @@ class SignUpHandler(webbapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(index_template.render())
 
+class ProfileHandler(webbapp2.RequestHandler):
+    def get(self):
+        index_template = jinja_env.get_template('templates/profile.html')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(index_template.render())
+
+class CreateHandler(webbapp2.RequestHandler):
+    def get(self):
+        index_template = jinja_env.get_template('templates/create.html')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(index_template.render())
+
+class ViewMessagesHandler(webbapp2.RequestHandler):
+    def get(self):
+        index_template = jinja_env.get_template('templates/view_messages.html')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(index_template.render())
+
+class PlayGameHandler(webbapp2.RequestHandler):
+    def get(self):
+        index_template = jinja_env.get_template('templates/play_game.html')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(index_template.render())
+
+
 app = webapp2.WSGIApplication([
     ('/', IndexHandler),
     ('/sign_up', SignUpHandler),
+    ('/profile', ProfileHandler),
+    ('/create', CreateHandler),
+    ('/view_messages' ViewMessagesHandler),
+    ('/play_game', PlayGameHandler),
 ], debug=True)
